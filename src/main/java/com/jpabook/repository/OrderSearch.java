a48ee140775521375da39c2452143bcca95a9b9c
sangwon <sangwon@sangwonui-MacBookPro.local>
2024-07-01 18:00:31 +0900

    주문검색기능개발

---
+++ src/main/java/com/jpabook/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package com.jpabook.repository;
+
+import com.jpabook.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+    private String memberName;
+    private OrderStatus orderStatus;
+}
