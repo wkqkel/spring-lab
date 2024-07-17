28ff6560bd17bfaa3e4ac0d43bf52d7aac33fdc7
wkqkel <wkqkel@naver.com>
2024-07-17 12:20:37 +0900

    글리스트 화면만들기

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
 
 const title = ref("")
 const content = ref("")
-
-const write = function(){
+const router = useRouter()
+const write = function () {
   axios.post("api/posts", {
     title: title.value,
     content: content.value
+  }).then(() => {
+    router.replace({name: "home"})
   })
 }
 
 </script>
 
 <template>
-  <el-input v-model="title"  placeholder="제목을 입력해주세요."/>
+  <el-input v-model="title" placeholder="제목을 입력해주세요."/>
   <div class="mt-2">
-    <el-input  v-model="content" type="textarea" rows="15"></el-input>
+    <el-input v-model="content" type="textarea" rows="15"></el-input>
   </div>
   <div class="mt-2">
     <el-button type="primary" @click="write()">글 작성완료</el-button>
